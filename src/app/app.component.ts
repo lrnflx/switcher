401ab409c678b8c0310b90431c6238d7e4c7a106
lrnflx <lorenefelx@gmail.com>
2019-02-25 17:15:12 +0100

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isAuth = false;
+  appareils: any[];
 
-  appareils = [
-    {
-      name: 'Machine à laver',
-      status: 'éteint'
-    },
-    {
-      name: 'Frigo',
-      status: 'allumé'
-    },
-    {
-      name: 'Ordinateur',
-      status: 'éteint'
-    }
-  ];
+  lastUpdate = new Promise((resolve,reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
 
-  constructor() { 
+
+  constructor(private appareilService: AppareilService) { 
     setTimeout(
       () => {
         this.isAuth = true;
       }, 4000
     );
+  }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
   }
 
   onAllumer(){
